077e1489d68c694334bffa8762a031482a1255f4
Coulibaly Abdourahmane <abdou.coulibaly@outlook.fr>
2022-07-30 20:47:40 +0200

    complete function done

--- js/script.js
+++ js/script.js
@@ -1,18 +1,46 @@
-const newTask=document.getElementById("add-task-input")
-const addBtn=document.getElementById("add-task-btn")
-const taskList=document.getElementById("task-list")
-addTask()
+const newTask = document.getElementById("add-task-input")
+const addBtn = document.getElementById("add-task-btn")
+const taskList = document.getElementById("task-list")
+manageAddBtn()
 
-function addTask(){
-    addBtn.addEventListener("click",function(event){
-        if(!newTask.value==""){
-            const task=`<li><div class="task-itm"><h2>${newTask.value}</h2><button class="del-task-btn"><i class="fa-solid fa-xmark"></i></button></div></li>`
-            taskList.innerHTML+=task
+function manageAddBtn() {
+    newTask.addEventListener('keypress', (event) => {
+        if (event.key === "Enter") {
+            addTask()
         }
-        console.log(newTask.value)
+    });
+    addBtn.addEventListener("click", function (event) {
+        addTask()
     })
 }
 
-function completeTask (){
-    
+function addTask() {
+    if (!newTask.value == "") {
+        const task = `<li><div class="task-itm"><h2 class="task-title">${newTask.value}</h2><button class="del-task-btn"><i class="fa-solid fa-xmark"></i></button></div></li>`
+        taskList.innerHTML += task
+        manageTask()
+    }
+}
+function delTask(a) {
+
+    a.remove()
+}
+function completeTask(a) {
+    a.classList.toggle("complete")
+    a.firstElementChild.classList.toggle("complete")
+}
+
+function manageTask() {
+    const taskItm = document.querySelectorAll("#task-list .task-itm")
+    const delBtns = document.querySelectorAll("#task-list .del-task-btn")
+    taskItm.forEach(itm => {
+        itm.addEventListener("click", function (event) {
+            completeTask(this)
+        })
+    });
+    delBtns.forEach(delBtn => {
+        delBtn.addEventListener("click", function (event) {
+            
+        })
+    })
 }
